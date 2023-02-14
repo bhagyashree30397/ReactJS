caee7d72564ff5d22029db792799cd4e076644f7
bhagyashree30397 <bhagyashree30397@gmail.com>
2023-02-14 18:36:28 +0530

    counter example using useState

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
+export default function App(){
 
-import axios from 'axios';
-import React from 'react'
-
-
-export default function App() {
- const [data,setData]=React.useState([]);
- React.useEffect(()=>{axios.get("https://jsonplaceholder.typicode.com/users")
- .then((response)=>{setData(response.data)}).catch(error=>console.log(error))},[])
- console.log("data",data);
-  return (
-   <>
-   <table>
-   <th>ID</th> <th>Name</th> <th>UserName</th><th>Website</th>
-    {data.map((a)=>{return <tr>
-      <td>{a.id}</td>
-      <td>{a.name}</td>
-      <td>{a.username}</td>
-      <td>{a.website}</td>
-      </tr>})}
-    </table>
-</>
+  const[counter , setCounter] =React.useState(0);
+  const increment=()=>{
+    setCounter(counter-1);
+  }
+  const decrement=()=>{
+    setCounter(counter+1);
+  }
+  return(
+  <>
+  <div>{counter}</div>
+  <input type="button" onClick={increment} value="Increment"/>
+  <input type="button" onClick={decrement} value="Decrement"/>
+  </>
   )
-}
+    
+  }
